a7ed896ec25e3f843f7426c3caef7a4815be4f43
KevNews03 <kevin.guillot03@outlook.fr>
2025-07-09 18:12:47 +0200

    Create package.js

---
+++ stratum/package.js
@@ -0,0 +1,11 @@
+{
+  "name": "kaspa-pool-stratum",
+  "version": "1.0.0",
+  "main": "stratum.js",
+  "scripts": {
+    "start": "node -r dotenv/config stratum.js"
+  },
+  "dependencies": {
+    "dotenv": "^16.0.3"
+  }
+}
